4dd424d9ad17d38802b74df9dd8913b96fbc0e81
Harry Sun <syfsyf@me.com>
2023-05-07 00:32:44 +0800

    created some functions

--- index.ts
+++ index.ts
@@ -1,14 +1,26 @@
-//声明 FetchEvent 和 Response 类型
-// declare global {
-//   interface FetchEvent extends Event {
-//     request: Request;
-//     respondWith(response: Promise<Response> | Response): void;
-//   }
-// }
+// 处理请求的函数
+const handler: ExportedHandler = {
+  async fetch(request: Request) {
+    // 构造 HTML 页面
+    const staticHTML = generateStaticHTML();
+    const dynamicJS = generateDynamicJS();
+    const dynamicCSS = generateDynamicCSS();
+    let html = staticHTML.replace('<script src="/dynamic.js"></script>', `<script>${dynamicJS}</script>`);
+    html = html.replace('<style src="/dynamic.css"></style>', `<style>${dynamicCSS}</style>`);
+
+    return new Response(html, {
+      headers: {
+        "content-type": "text/html;charset=UTF-8",
+      },
+    });
+  },
+};
+
+export default handler;
 
 // 生成 HTML 静态部分的函数
 function generateStaticHTML() {
-    return `
+  return `
       <!DOCTYPE html>
       <html lang="en">
         <head>
@@ -23,42 +35,31 @@ function generateStaticHTML() {
           <footer><div class="footer">Powered by<a class="ui label" href="https://github.com/AHA1GE/cf-worker-dir-remasterd" target="_blank" rel="noopener"><i class="github icon"></i>Cf-Worker-Dir</a> © Based on<a class="ui label"><i class="balance scale icon"></i>MIT License</a></div></footer>
   
           <script src="/dynamic.js"></script>
-          <style></style>
+          <style src="/dynamic.css></style>
         </body>
       </html>
     `;
-  }
-  
-  // 生成用于更新动态 div 的 JavaScript 的函数
-  function generateDynamicJS() {
-    return `
-      window.addEventListener('load', () => {
+}
+// 存储用于更新动态 div 的 JavaScript 的静态部分的常数
+const staticJS = `window.addEventListener('load', () => {
         const topPartOfHtml = document.getElementById('top-part-of-html');
         const mainPartOfHtml = document.getElementById('main-part-of-html');
   
         topPartOfHtml.textContent = 'This is the first dynamic div.';
         mainPartOfHtml.textContent = 'This is the second dynamic div.';
       });
-    `;
-  }
-  
-  // 处理请求的函数
-  async function handleRequest(event : FetchEvent) {
-    // 构造 HTML 页面
-    const staticHTML = generateStaticHTML();
-    const dynamicJS = generateDynamicJS();
-    const html = staticHTML.replace('<script src="/dynamic.js"></script>', `<script>${dynamicJS}</script>`);
-  
-    const response = new Response(html, {
-      status: 200,
-      headers: {
-        'Content-Type': 'text/html',
-      },
-    });
-    event.respondWith(response);
-  }
-  
-  addEventListener('fetch', (event) => {
-    handleRequest(event);
-  });
-  
+      `
+// 生成用于更新动态 div 的 JavaScript 的函数
+function generateDynamicJS() {
+  const dynamicJS1 = ``;
+  const dynamicJS2 = ``;
+  return staticJS.replace(`'This is the first dynamic div.'`, dynamicJS1).replace(`'This is the second dynamic div.'`, dynamicJS2);
+}
+
+//生成用于更新动态 css 的函数
+function generateDynamicCSS() {
+  return `
+  `
+}
+
+
